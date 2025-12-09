b6e9ee8d4a25943ebfc6dd0617806ff0592dad88
Ayusha200333 <ayushawijerathna@gmail.com>
2025-12-09 15:54:31 +0530

    add search bar

--- src/components/Common/Navbar.tsx
+++ src/components/Common/Navbar.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { HiOutlineUser,HiOutlineShoppingBag } from 'react-icons/hi'
 import { HiBars3BottomRight } from 'react-icons/hi2'
 import { Link } from 'react-router-dom'
+import SearchBar from './SearchBar'
+import Cart from '../Layout/Cart'
 
 const Navbar = () => {
+    const [drawerOpen , setDrawerOpen] = useState(false)
+    
+        const toogleCart = () => {
+            setDrawerOpen(!drawerOpen)
+        }
+
   return (
     <>
         <nav className='container mx-auto flex items-center justify-between py-4 px-6'>
@@ -26,20 +34,30 @@ const Navbar = () => {
                     Backpacks
                 </Link>
             </div>
+
             <div className="flex items-center space-x-4">
                 <Link to="/profile" className='hover:text-black'>
                     <HiOutlineUser className='h-6 w-6 text-gray-700'/>
                 </Link>
-                <button className='relative hover:text-black'>
+
+                <button onClick={toogleCart} className='relative hover:text-black'>
                     <HiOutlineShoppingBag className='h-6 w-6 text-gray-700'/>
-                    <span className='absolute -top-1 bg-[tomato] text-white text-xs rounded-full px-2 py-0.5'>4</span>
+                    <span className='absolute -top-1 bg-[tomato] text-white text-xs rounded-full px-2 py-0.5'>
+                        4
+                    </span>
                 </button>
+
+                <div className='overflow-hidden'>
+                   <SearchBar/>
+                </div>
 
                 <button className='md:hidden'>
                     <HiBars3BottomRight className='h-6 w-6 text-gray-700'/>
                 </button>
             </div>
         </nav>
+
+        <Cart drawerOpen={drawerOpen} toogleCart={toogleCart}/>
     </>
   )
 }
