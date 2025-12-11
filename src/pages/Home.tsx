b8f870cce2c87fc271df322b8b1cb9de635017aa
Ayusha200333 <ayushawijerathna@gmail.com>
2025-12-11 14:12:51 +0530

    add product details section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Layout/Hero'
 import MenCollection from '../components/Products/MenCollection'
 import NewCollections from '../components/Products/NewCollections'
+import ProductDetails from '../components/Products/ProductDetails'
 
 const Home = () => {
   return (
@@ -13,6 +14,8 @@ const Home = () => {
         <h2 className='text-3xl text-center font-bold mb-4'>
           Trending Now
         </h2>
+
+        <ProductDetails/>
     </div>
   )
 }
