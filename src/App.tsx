a9be5a666cbce61392ae9046a8882f2f9c3c3355
Ayusha200333 <ayushawijerathna@gmail.com>
2025-12-13 21:16:25 +0530

    add filter side bar

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { Toaster } from 'sonner'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import ProfilePage from './pages/ProfilePage'
+import CollectionPage from './pages/CollectionPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,7 +23,7 @@ function App() {
         <Route path='login' element={<LoginPage/>} />
         <Route path='register' element={<RegisterPage/>} />
         <Route path='profile' element={<ProfilePage/>} />
-
+        <Route path='collections/:collection' element={<CollectionPage/>} />
       </Route>
       <Route>{/*Admin layout*/}</Route>
     </Routes>
